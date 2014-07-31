cd4947fd769435f18cd61bbfeb5beb58c68511dd
James Patterson <james@m1dst.co.uk>
2014-07-31 14:58:01 +0100

    Initial check in of clean .NET solution

---
+++ Presentation/DSTcontest.Web/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace DSTcontest.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
